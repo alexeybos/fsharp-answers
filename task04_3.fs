0d730e4f454e4351a2fc4d544d3e6914337f5d33
Alexey Bosalyga <alexey.bos@gmail.com>
2026-08-30 22:37:09 +0400

    Add files via upload

---
+++ task04_3.fs
@@ -0,0 +1,14 @@
+﻿[<Fact>]
+let ``10-е число Фибоначчи должно быть 55`` () =
+    let actual = fibo 10
+    Assert.Equal(55, actual)
+
+[<Fact>]
+let ``Сумма чисел от 0 до 10 должна быть 55`` () =
+    let actual = sum 10
+    Assert.Equal(55, actual)
+
+[<Fact>]
+let ``Сумма чисел для 2, 2 должна быть 9`` () =
+    let actual = sum2 (2, 2)
+    Assert.Equal(9, actual)
