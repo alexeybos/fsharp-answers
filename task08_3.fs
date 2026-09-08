63ba692545ea13d6566d128f025a38f979f0c048
Alexey Bosalyga <alexey.bos@gmail.com>
2026-09-08 13:30:55 +0400

    Add files via upload

--- task08_3.fs
+++ task08_3.fs
@@ -4,6 +4,7 @@ let ``carry для add должно быть 12`` () =
     let actual = curry add 10 2
     Assert.Equal(12, actual)
 
+[<Fact>]
 let ``uncarry для add должно быть 12`` () =
     let add x y = x + y
     let actual = uncurry add (10, 2)
