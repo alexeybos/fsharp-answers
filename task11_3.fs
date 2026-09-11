22a303a6271b249c01c4a3b4804c1f1728e8aaf0
Alexey Bosalyga <alexey.bos@gmail.com>
2026-09-11 22:00:45 +0400

    Add files via upload

---
+++ task11_3.fs
@@ -0,0 +1,41 @@
+﻿[<Fact>]
+let ``10:36 PM .>. 10:36 AM должно быть true`` () =
+    let fdate = { hours = 10; minutes = 36; f = PM }
+    let sdate = { hours = 10; minutes = 36; f = AM }
+    let actual = fdate .>. sdate
+    Assert.True(actual)
+
+[<Fact>]
+let ``10:36 AM .>. 10:36 PM должно быть false`` () =
+    let fdate = { hours = 10; minutes = 36; f = PM }
+    let sdate = { hours = 10; minutes = 36; f = AM }
+    let actual = sdate .>. fdate
+    Assert.False(actual)
+
+[<Fact>]
+let ``10:36 AM .>. 10:36 AM должно быть false`` () =
+    let fdate = { hours = 10; minutes = 36; f = AM }
+    let sdate = { hours = 10; minutes = 36; f = AM }
+    let actual = sdate .>. fdate
+    Assert.False(actual)
+
+[<Fact>]
+let ``10:37 AM .>. 10:36 AM должно быть true`` () =
+    let fdate = { hours = 10; minutes = 37; f = AM }
+    let sdate = { hours = 10; minutes = 36; f = AM }
+    let actual = fdate .>. sdate
+    Assert.True(actual)
+
+[<Fact>]
+let ``2:37 AM .>. 10:36 AM должно быть false`` () =
+    let fdate = { hours = 2; minutes = 37; f = AM }
+    let sdate = { hours = 10; minutes = 36; f = AM }
+    let actual = fdate .>. sdate
+    Assert.False(actual)
+
+[<Fact>]
+let ``9:20 AM .>. 5:36 AM должно быть true`` () =
+    let fdate = { hours = 9; minutes = 20; f = AM }
+    let sdate = { hours = 5; minutes = 36; f = AM }
+    let actual = fdate .>. sdate
+    Assert.True(actual)
