6ae6a5d1dbdd52d6d6ccedb2acb9e5f4113dacce
Alexey Bosalyga <alexey.bos@gmail.com>
2026-09-19 13:25:13 +0400

    Add files via upload

---
+++ task14.fs
@@ -0,0 +1,94 @@
+// 40.1
+let rec sum (p, xs) = 
+  let rec compute = function
+   | [] -> 0
+   | head :: tail when p head -> head + compute tail
+   | head :: tail -> compute tail
+  compute xs
+  
+// 40.2.1
+let rec count (xs, n) = 
+ match xs with
+  | [] -> 0
+  | head :: tail when head > n -> 0
+  | head :: tail when head = n -> 1 + count(tail, n)
+  | head :: tail -> count(tail, n)
+  
+// 40.2.2
+let rec insert (xs, n) = 
+ let rec spl = function
+  | (h, []) -> h @ [n]
+  | (h, [x]) when x >= n -> h @ [n] @ [x]
+  | (h, [x]) when x < n -> h @ [x] @ [n]
+  | (h, head :: (head2 :: _ as tail)) when head < n && head2 >= n -> h @ [head] @ [n] @ tail
+  | (h, head :: tail) -> spl (h @ [head], tail)
+ spl ([], xs)
+
+// 40.2.3
+let rec intersect (xs1, xs2) = 
+ let rec slide = function
+  | ([], _, r) | (_, [], r) -> List.rev r
+  | (head1 :: tail1,head2 :: tail2,r) when head1 = head2 -> slide (tail1, tail2, head1 :: r)
+  | (head1 :: tail1,head2 :: tail2,r) when head1 < head2 -> slide (tail1, head2 :: tail2, r)
+  | (head1 :: tail1,head2 :: tail2,r) when head1 > head2 -> slide (head1 :: tail1, tail2, r)
+ slide (xs1, xs2, [])
+
+// 40.2.4
+let rec plus (xs1, xs2) = 
+ let rec slide = function
+  | ([], [], r) -> List.rev r
+  | ([], l, r) | (l, [], r) -> List.rev r @ l
+  | (head1 :: tail1,head2 :: tail2,r) when head1 <= head2 -> slide (tail1, head2 :: tail2, head1 :: r)
+  | (head1 :: tail1,head2 :: tail2,r) when head1 > head2 -> slide (head1 :: tail1, tail2, head2 :: r)
+ slide (xs1, xs2, [])
+
+// 40.2.5
+let rec minus (xs1, xs2) = 
+ let rec slide = function
+  | ([], _, r) -> List.rev r
+  | (l, [], r) -> List.rev r @ l
+  | (head1 :: tail1,head2 :: tail2,r) when head1 = head2 -> slide (tail1, tail2, r)
+  | (head1 :: tail1,head2 :: tail2,r) when head1 < head2 -> slide (tail1, head2 :: tail2, head1 :: r)
+  | (head1 :: tail1,head2 :: tail2,r) when head1 > head2 -> slide (head1 :: tail1, tail2, r)
+ slide (xs1, xs2, [])
+
+// 40.3.1
+let rec smallest xs = 
+ let rec min = function
+  | ([], m) -> m
+  | (head :: tail, m) when head < m -> min (tail, head)
+  | (head :: tail, m) -> min (tail, m)
+ match xs with
+   | [] -> None
+   | [head] -> Some(head)
+   | head :: tail -> Some(min (tail, head))
+
+// 40.3.2
+let rec delete (n, xs) = 
+ let rec slide = function
+  | ([], r) -> r
+  | ([head], r) when head = n -> r
+  | ([head], r) -> List.rev (head :: r)
+  | (head :: tail, r) when head = n -> List.rev (r) @ tail
+  | (head :: tail, r) -> slide (tail, head :: r)
+ slide (xs, [])
+ 
+// 40.3.3
+let rec sort xs = 
+ let apply_min m l r = (delete (m, l), m :: r)
+ let rec proc = function
+  | ([], r) -> List.rev r
+  | ([head], r) -> List.rev (head :: r)
+  | (l, r) -> proc (apply_min (Option.get (smallest l)) l r)
+ proc (apply_min (Option.get (smallest xs)) xs [])
+ 
+// 40.4
+let rec revrev xs =  
+ let rec rev_inner = function
+  | head :: tail -> (rev_inner tail) @ [head]
+  | [] -> []
+ match xs with
+  | head :: tail -> (revrev tail) @ [rev_inner head]
+  | [] -> []
+ 
+
---
+++ task14_3.fs
@@ -0,0 +1,60 @@
+﻿[<Fact>]
+let ``sum для четных чисел списка равно 6`` () =
+   let actual = sum (is_even, [1 .. 5])
+   Assert.Equal(6, actual)
+
+[<Fact>]
+let ``count для элемента 5 в списке [1;3;5;5;7] равно 2`` () =
+   let actual = count ([1;3;5;5;7], 5)
+   Assert.Equal(2, actual)
+
+[<Fact>]
+let ``insert 4 в слабовосходящий список [1;3;5;8] дает [1;3;4;5;8]`` () =
+   let actual = insert ([1;3;5;8], 4)
+   Assert.Equal<int list>([1;3;4;5;8], actual)
+
+
+[<Fact>]
+let ``intersect ([1;3;5;8], [4]) дает []`` () =
+   let actual = intersect ([1;3;5;8], [4])
+   Assert.Equal<int list>([], actual)
+
+[<Fact>]
+let ``intersect ([1;3;3;3;5;8], [3;3;5;6]) дает [3;3;5]`` () =
+   let actual = intersect ([1;3;3;3;5;8], [3;3;5;6])
+   Assert.Equal<int list>([3;3;5], actual)
+
+[<Fact>]
+let ``plus ([1;3;5;8], [4]) дает [1;3;4;5;8]`` () =
+   let actual = plus ([1;3;5;8], [4])
+   Assert.Equal<int list>([1;3;4;5;8], actual)
+
+[<Fact>]
+let ``minus ([1;3;3;5;8], [3;5;6]) дает [1;3;8]`` () =
+   let actual = minus ([1;3;3;5;8], [3;5;6])
+   Assert.Equal<int list>([1;3;8], actual)
+
+[<Fact>]
+let ``smallest [7;12;5;8] дает 5`` () =
+   let actual = Option.get (smallest [7;12;5;8])
+   Assert.Equal(5, actual)
+
+[<Fact>]
+let ``delete (3, [1 .. 5]) дает [1;2;4;5]`` () =
+   let actual = delete (3, [1 .. 5])
+   Assert.Equal<int list>([1;2;4;5], actual)
+
+[<Fact>]
+let ``sort [7;12;5;8] дает [5;7;8;12]`` () =
+   let actual = sort [7;12;5;8]
+   Assert.Equal<int list>([5;7;8;12], actual)
+
+[<Fact>]
+let ``revrev [[1;2];[3;4;5];[6;7]] дает [[7; 6]; [5; 4; 3]; [2; 1]]`` () =
+   let actual = revrev [[1;2];[3;4;5];[6;7]]
+   let actual_a = actual.[0]
+   let actual_b = actual.[1]
+   let actual_c = actual.[2]
+   Assert.Equal<int list>([7; 6], actual_a)
+   Assert.Equal<int list>([5; 4; 3], actual_b)
+   Assert.Equal<int list>([2; 1], actual_c)
